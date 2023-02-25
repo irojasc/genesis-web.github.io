99e7db555968f659a3c784bc02a41c070769be87
IROJASC <irojasc3@gmail.com>
2023-02-25 07:36:42 -0500

    Primera actualizacion

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const btn1 = document.getElementById('btn1');
+/* const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
 const allImages = document.querySelector('.main-slider-images');
@@ -45,5 +45,5 @@ function cambiar(){
     }
 }
 
-window.setInterval(cambiar, 4000);
+window.setInterval(cambiar, 4000); */
 
